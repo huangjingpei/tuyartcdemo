f545ec3cee2e97a5a267bd4dcdfc50bbb071032f
huangjingpei <huangjingpei@gmail.com>
2021-04-12 11:33:36 +0800

    fix

--- app/src/main/java/com/tuya/tuyartcdemo/MainActivity.java
+++ app/src/main/java/com/tuya/tuyartcdemo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tuya.tuyartcdemo;
 
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,10 +9,15 @@ import com.google.android.material.snackbar.Snackbar;
 import com.tuya.rtc.TuyaRTCEngine;
 import com.tuya.rtc.TuyaRTCEngineFactory;
 import com.tuya.rtc.TuyaRTCEngineHandler;
+import com.tuya.rtc.TuyaVideoRenderer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -19,11 +26,18 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
 
 import org.webrtc.EglBase;
+import org.webrtc.RendererCommon;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener ,
         CompoundButton.OnCheckedChangeListener{
+    private static final String TAG = "MainActivity";
+
+    private static final String[] MANDATORY_PERMISSIONS = {"android.permission.CAMERA",
+            "android.permission.RECORD_AUDIO", "android.permission.WRITE_EXTERNAL_STORAGE"};
+
     private Button sdkInitBtn;
     private Button startPreivewBtn;
     private Button subscribeTopicBtn;
@@ -36,6 +50,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private LayoutInflater inflater;
 
     private EglBase eglBase;
+    private TuyaRTCEngine rtcEngine;
+
+    private RelativeLayout localSurfaceLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,20 +78,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         muteLocalAudiocheckbox.setOnCheckedChangeListener(this);
         muteLocalVideocheckbox.setOnCheckedChangeListener(this);
+
+        localSurfaceLayout = (RelativeLayout)findViewById(R.id.rtc_local_surfaceview);
     }
 
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.sdkInit) {
+            requestDangerousPermissions(MANDATORY_PERMISSIONS, 1);
             eglBase = EglBase.create(null /* sharedContext */, EglBase.CONFIG_PLAIN);
-            TuyaRTCEngineFactory.getInstance().createEngine(this, eglBase, new TuyaRTCEngineHandler(){
+            rtcEngine = TuyaRTCEngineFactory.getInstance().createEngine(this, eglBase, new TuyaRTCEngineHandler(){
                 @Override
                 public void onUserJoin(String s) {
 
                 }
             });
+            Log.d(TAG, "onClick() called with: v = [" + v + "]");
         } else if (v.getId() == R.id.startPreview) {
+            TuyaVideoRenderer viewRenderer = new TuyaVideoRenderer(this);
+            viewRenderer.setKeepScreenOn(true);
+            viewRenderer.setZOrderMediaOverlay(true);
+            viewRenderer.setZOrderOnTop(false);
+            viewRenderer.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT);
+            //viewRenderer.init(eglBase.getEglBaseContext(), null);
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(1280, 720);
+            viewRenderer.setLayoutParams(layoutParams);
+            localSurfaceLayout.setVisibility(View.VISIBLE);
+            localSurfaceLayout.addView(viewRenderer);
+            rtcEngine.startPreview("", 1280, 720, 30, viewRenderer, null);
 
         } else if (v.getId() == R.id.subscribeTopic) {
 
@@ -87,4 +119,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
     }
+
+
+    /**
+     * 请求权限
+     */
+    public void requestDangerousPermissions(String[] permissions, int requestCode) {
+        if (checkDangerousPermissions(permissions)){
+            handlePermissionResult(requestCode, true);
+            return;
+        }
+        ActivityCompat.requestPermissions(this, permissions, requestCode);
+    }
+
+    /**
+     * 检查是否已被授权危险权限
+     * @param permissions
+     * @return
+     */
+    private boolean checkDangerousPermissions(String[] permissions) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        boolean granted = true;
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                granted = false;
+            }
+        }
+        boolean finish = handlePermissionResult(requestCode, granted);
+        if (!finish){
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    /**
+     * 处理请求危险权限的结果
+     * @return
+     */
+    private boolean handlePermissionResult(int requestCode, boolean granted) {
+        //Notice 这里要自定义处理权限申请。
+        return false;
+    }
+
 }
